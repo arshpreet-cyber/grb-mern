3bf711392ed24d48ec9daae2a45710a97d0179cf
Sourabh Daura <sourabh.daura@gmail.com>
2026-04-06 11:25:35 +0530

    fix: remove curly braces from prisma import

--- app/api/reviews/route.ts
+++ app/api/reviews/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { prisma } from '../../../lib/prisma';
+import prisma from '../../../lib/prisma';
 
 // GET: Fetch all reviews from the database
 export async function GET() {
