cdced2a6925b1c80f14d8c2803b4a3c82b7cea94
sourabhdaura <sourabh.daura@gmail.com>
2026-04-23 13:57:01 +0530

    dashboard

--- components/UserSidebar.tsx
+++ components/UserSidebar.tsx
@@ -66,7 +66,7 @@ const menuSections: MenuSection[] = [
         id: "administration",
         subItems: [
           { label: "Account Management", href: "/dashboard/account" },
-          { label: "Finance", href: "/dashboard/admin/finance" },
+          // { label: "Finance", href: "/dashboard/admin/finance" },
         ]
       },
     ],
