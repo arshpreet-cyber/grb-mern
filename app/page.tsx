88f0e76bdda4e997daa86ce89c8c4830bab40f98
sourabhdaura <sourabh.daura@gmail.com>
2026-04-27 15:30:04 +0530

    Integrate product grid into Home Page lead to match requested design

--- app/page.tsx
+++ app/page.tsx
@@ -16,12 +16,15 @@ import {
 
 import Wrapper from "@/components/Wrapper";
 
+import { BuyReviewsSection } from "@/components/BuyReviewsSection";
+
 export default function HomePage() {
   return (
     <Wrapper>
       <div className="min-h-screen bg-white text-slate-900 font-sans">
         <HomeNavbar />
         <HeroBanner />
+        <BuyReviewsSection />
         <StatsBar />
         <IconGrid />
         <ServicesSection />
